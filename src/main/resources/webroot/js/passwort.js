d5917c1d5388a0a778c74da2d4ada7560d950926
k.engler <k.engler@IR1-PC-18.musterschule.schule.paedml>
2017-12-11 17:27:44 +0100

    Informatik Projekt Registrierungsknopf eingefügt

--- src/main/resources/webroot/js/passwort.js
+++ src/main/resources/webroot/js/passwort.js
@@ -20,6 +20,24 @@ $(document).ready(function () {
               if (data.text=="ok"){
                   $("body").html("Gratulation, du bist angemeldet!")
                           .append("<br><input type='button' value='logout' id='logout'/>");
+                          
+              } else {
+                  $("body").append("<br>Die Anmeldedaten waren leider falsch!");
+              }
+          } 
+       }); 
+    });
+    $(document).on("click","#anmeldeknopf",function(){
+       $.post("../anfrage",{
+           typ: "anmeldedaten",
+           anmeldename: $("#anmeldename").val(),
+           passwort: $("#passwort").val()
+       }, function(data){
+          if (data.typ=="überprüfung"){
+              if (data.text=="ok"){
+                  $("body").html("Gratulation, du bist angemeldet!")
+                          .append("<br><input type='button' value='logout' id='logout'/>");
+                          
               } else {
                   $("body").append("<br>Die Anmeldedaten waren leider falsch!");
               }
@@ -37,8 +55,8 @@ $(document).ready(function () {
                     if (data.text=="nein"){
                         $("body").html("Name: <input type='text' id='anmeldename'/><br>")
                         .append("Passwort: <input type='password' id='passwort'/><br>\n")
-                        .append("<input type='button' value='OK' id='anmeldeknopf'/>")
-                        
+                        .append("<input type='button' value='OK' id='anmeldeknopf'/><br>")
+                        .append("<input type='button' value='Registrieren' id='Registrierungsknopf'/>");
                     } else {
                         $("body").html("Gratulation, du bist angemeldet!")
                           .append("<br><input type='button' value='logout' id='logout'/>");
